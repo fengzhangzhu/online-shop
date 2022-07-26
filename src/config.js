396a43fac6194ae3a8cb8cfb83062cc8e2491922
fengzhangzhu <1278343591@qq.com>
2022-07-26 17:17:01 +0800

    front end code

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export default {
+    baseURL: 'http://myadmin.test/api/',
+    header: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "X-Requested-With",
+        "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS",
+        "X-Powered-By": "3.2.1",
+        "Content-Type": "application/json;charset=utf-8",
+    }
+}
+
